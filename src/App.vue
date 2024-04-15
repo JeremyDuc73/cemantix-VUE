c9ee0d1cac4c2b5eeac658b445a42d3a577e26aa
jeremy <nashoba.jeremy@gmail.com>
2024-04-15 17:18:25 +0200

    oui

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import {ref, watchEffect, reactive} from "vue";
+const word = ref("")
+const inputWord = ref("")
+const compareValue = ref(null)
+const wordsUsed = reactive([])
+const values = reactive([])
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+watchEffect(async () => {
+  const url = `https://127.0.0.1:8000/fetch/${word.value}`
+  compareValue.value = await (await fetch(url)).json()
+  if (!values.includes(compareValue.value))
+  {
+    values.push(compareValue.value)
   }
+})
+
+function sendWord(){
+  word.value = inputWord.value
+  if (!wordsUsed.includes(word.value))
+  {
+    wordsUsed.push(word.value)
+  }
+
 }
 </script>
 
+
+<template>
+  <input v-model.lazy="inputWord">
+  <button @click="sendWord">Send</button>
+  <p v-if="!compareValue">Loading...</p>
+  <pre v-else>{{compareValue}}</pre>
+
+  <div v-for="w in wordsUsed" v-bind:key="w.value">
+    <p>{{w}}</p>
+  </div>
+  <div v-for="v in values" v-bind:key="v.value">
+    <p>{{v}}</p>
+  </div>
+</template>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
